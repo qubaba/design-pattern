7c9efed49f6ada5d928a27324611e25037027bd1
qp <1020081257@qq.com>
2018-07-25 21:42:08 +0800

    初始化提交

---
+++ simple-factory/src/main/java/com/qubaba/simplefactory/SimpleFactoryApplication.java
@@ -0,0 +1,12 @@
+package com.qubaba.simplefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleFactoryApplication.class, args);
+    }
+}
